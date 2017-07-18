a4dab0eaef0f91b7f8071ed3258e1f8968786915
Unknown <bm1952@nyu.edu>
2017-07-18 12:16:32 -0400

    Added first version of game

---
+++ Assets/Scripts/OpenEyes.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenEyes : MonoBehaviour {
+
+	public Sprite[] eyeSprites;
+	public GameObject eyes;
+	public AudioSource music;
+
+	void OnCollisionEnter() {
+		eyes.GetComponent<SpriteRenderer>().sprite = eyeSprites[1];
+		music.Play();
+	}
+
+}
---
+++ Assets/Scripts/StartInput.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartInput : MonoBehaviour {
+
+	public float MoveSpeed;
+
+	void Update () {
+		if (Input.GetKey(KeyCode.Space)) {
+			gameObject.transform.Translate(new Vector3 (0f, 0f, -MoveSpeed));
+		}
+	}
+}
